88eef4064425d5f452435ed1f397c4e227656440
IaraFV <103749046+IaraFV@users.noreply.github.com>
2022-08-19 16:41:57 -0300

    up

--- src/Pages/Login/styles.js
+++ src/Pages/Login/styles.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
-import login1 from '../../assets/login1.png';
-import Login2 from '../../assets/Login2.png'
+import Login2 from '../../assets/Login2.png';
+import teste from '../../assets/teste.png';
 
 export const Container = styled.div`
     display: flex;
@@ -8,28 +8,31 @@ export const Container = styled.div`
 `;
 
 export const BackgroundForm = styled.div`
-    background-image: url(${login1}); 
+    background-image: url(${teste}); 
     display: flex;
     height: 100vh;
     background-repeat: no-repeat; 
-    width: 50vw;  
+    width: 110vh;  
 `;
+
 
 export const IllustrationForm = styled.div`
     background-image: url(${Login2});
     display: flex;
-    height: 50vh;
+    height: 48vh;
     background-repeat: no-repeat; 
     position: relative;
-    width: 50vw;     
-    margin-top: 28%; 
-    margin-left: 12%;
+    width: 35vw;     
+    margin-top: 35%; 
+    margin-left: 21%;
+   
+   
 `;
 
 export const Title = styled.p`
     display: flex;
     justify-content: center;
-    width: 20rem;
+    width: 22rem;
     font-family: 'Mulish';
     font-style: normal;
     font-weight: 400;
