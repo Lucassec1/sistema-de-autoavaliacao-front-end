13d60f2fea0f9f4444b6c8738339b008da4fac3c
Lucasmartinsn <106749361+Lucasmartinsn@users.noreply.github.com>
2022-08-25 16:18:10 -0300

    formulario

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import  Login  from './Pages/Login/Login';
 import Rotas from "./Routes.js"
 import { AuthProvider } from "./Components/Context/AuthContext";
 
+
 function App() {
   return (
       <div style={{display: 'flex'}}>
