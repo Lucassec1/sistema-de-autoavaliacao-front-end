8e9c2b4eb3b49df551dfcb999915c453f7e6cc4c
IaraFV <103749046+IaraFV@users.noreply.github.com>
2022-08-25 16:17:15 -0300

    up

--- src/Components/NavBar/navbar.js
+++ src/Components/NavBar/navbar.js
@@ -1,13 +1,31 @@
 import React from 'react'
-import { HeaderHome } from './styles'
-
+import { HeaderHome } from './styles';
+import { CardHome } from './styles';
+import { Card, Col, Row } from 'antd';
 export default function NavBarHome() {
     return (
         <>
             <HeaderHome class="navbar bg-light">
-                <div class="container-fluid">
-                    <span class="navbar-brand mb-0 h1"></span>
-                </div>
+                <h1 style={{color: '#fff'}}>Dashboard</h1>
+                <CardHome class="container text-center">
+                    <Row gutter={16}>
+                        <Col span={8}>
+                            <Card title="Card title" bordered={false}>
+                                Card content
+                            </Card>
+                        </Col>
+                        <Col span={8}>
+                            <Card title="Card title" bordered={false}>
+                                Card content
+                            </Card>
+                        </Col>
+                        <Col span={8}>
+                            <Card title="Card title" bordered={false}>
+                                Card content
+                            </Card>
+                        </Col>
+                    </Row>
+                </CardHome>
             </HeaderHome>
         </>
     )
