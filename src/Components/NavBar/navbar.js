599bf4fd05811285ad1c3ea3e15a8627586ca5b1
IaraFV <103749046+IaraFV@users.noreply.github.com>
2022-08-25 15:35:22 -0300

    construindo navbar dashboard

---
+++ src/Components/NavBar/navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { HeaderHome } from './styles'
+
+export default function NavBarHome() {
+    return (
+        <>
+            <HeaderHome class="navbar bg-light">
+                <div class="container-fluid">
+                    <span class="navbar-brand mb-0 h1"></span>
+                </div>
+            </HeaderHome>
+        </>
+    )
+}
---
+++ src/Components/NavBar/styles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const HeaderHome = styled.div`
+    background: rgba(52, 122, 226, 0.7);
+    display: flex;
+    width: 100%;
+    height: 28vh;
+`;
