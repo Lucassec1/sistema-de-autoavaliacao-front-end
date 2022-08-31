c5bcbb8463d5e91c2e701dd20a9f268bffb36597
dayannebugarim <81201377+dayannebugarim@users.noreply.github.com>
2022-08-31 14:15:21 -0300

    Ajuste no input do login (cpf)

--- src/Pages/Login/Components/Inputinicial/Input.js
+++ src/Pages/Login/Components/Inputinicial/Input.js
@@ -93,14 +93,17 @@ const NumericInput = (props) => {
         autoComplete="off"
       >
         <Form.Item
+          type="number"
           label="CPF"
           name="cpf"
-          
-          
           rules={[
             {
               required: true,
               message: "O CPF é obrigatório!",
+            }, 
+            {
+              pattern: /^(?:\d*)$/,
+              message: "O CPF deve conter apenas números!",
             },
           ]}
           value={cpf}
@@ -109,7 +112,7 @@ const NumericInput = (props) => {
             setCpf(e.target.value);
           }}
         >
-          <InputNumber keyboard placeholder='Digite seu CPF' style={{width: '235px'}}/>
+          <Input maxLength={11} minLength={11} placeholder='Digite seu CPF' style={{width: '235px'}}/>
         </Form.Item>
 
         <Form.Item
@@ -132,6 +135,7 @@ const NumericInput = (props) => {
 
         <ButtonContainer>
           <LoginButton
+            htmlType="submit"
             type="primary"
             loading={loading}
             size="large"
