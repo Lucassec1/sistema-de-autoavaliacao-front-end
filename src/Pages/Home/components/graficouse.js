dd7bc139a42aa0fcd6ccf06aff71684ebe894ace
Lucassec1 <106749169+Lucassec1@users.noreply.github.com>
2022-08-31 15:35:31 -0300

    home

--- src/Pages/Home/components/graficouse.js
+++ src/Pages/Home/components/graficouse.js
@@ -1,10 +1,83 @@
 import React from 'react';
-
-//import { Bar } from 'react-chartjs'
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend
+} from "recharts";
 function Graficouse() {
-    return(
-        <>
-        </>
-    )
+    const data = [
+        {
+          name: "Page A",
+      
+          pv: 2400,
+          amt: 2400
+        },
+        {
+          name: "Page B",
+         
+          pv: 1398,
+          amt: 2210
+        },
+        {
+          name: "Page C",
+        
+          pv: 9800,
+          amt: 2290
+        },
+        {
+          name: "Page D",
+      
+          pv: 3908,
+          amt: 2000
+        },
+        {
+          name: "Page E",
+          
+          pv: 4800,
+          amt: 2181
+        },
+        {
+          name: "Page F",
+          pv: 3800,
+          amt: 2500
+        },
+        {
+          name: "Page G",
+          
+          pv: 4300,
+          amt: 2100
+        }
+      ];
+      
+        return (
+          <LineChart
+            width={700}
+            height={500}
+            data={data}
+            margin={{
+              top: 5,
+              right: 33,
+              left: -2,
+              bottom: 5
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Line
+              type="monotone"
+              dataKey="pv"
+              stroke="#163A6F"
+              activeDot={{ r: 8 }}
+            />
+          </LineChart>
+        );
 }
+
 export default Graficouse;
--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,10 +1,11 @@
 import NavBarHome from "../../Components/NavBar/navbar"
-import { CardUsuarios } from "./styles"
-import { Grafico } from "./styles"
-import { NomeUsuario } from "./styles"
+import { Grafico, NomeUsuario, CardUsuarios, HeaderRenderUsusarios } from "./styles"
 import Graficouse from './components/graficouse';
 import { BsFillPersonFill } from "react-icons/bs";
 import { FiArrowUpRight, FiArrowDownLeft } from "react-icons/fi";
+
+
+
 export default function Home() {
 
     return (
@@ -12,7 +13,7 @@ export default function Home() {
             <NavBarHome />
             <CardUsuarios class="row justify-content-around">
                 <div class="col-6">
-
+                    <HeaderRenderUsusarios>Analise de usuarios</HeaderRenderUsusarios>
                     <ul>
                         <li style={{ listStyle: 'none', display: 'flex', marginLeft: '-6%', width: '49rem', padding: '3%', borderBottom: ' 1px solid rgba(37, 38, 40, 0.2)' }}>
                             <BsFillPersonFill style={{ background: 'rgba(33, 126, 253, 0.298039)', borderRadius: '6px', width: '30px', height: '25px', color: '#217EFD' }} />
@@ -20,11 +21,36 @@ export default function Home() {
                             <FiArrowUpRight style={{ background: 'rgba(26, 213, 152, 0.2)', color: '#1AD598', width: '3.2%', height: '24px', borderRadius: '10rem', marginLeft: '30rem' }} />
                             <p style={{ color: '#1AD598' }}>+ 7,44%</p>
                         </li>
-                        <li style={{ listStyle: 'none', display: 'flex', marginLeft: '-6%', width: '48rem', padding: '3%' }}>
+
+                        <li style={{ listStyle: 'none', display: 'flex', marginLeft: '-6%', width: '49rem', padding: '3%', borderBottom: ' 1px solid rgba(37, 38, 40, 0.2)' }}>
+
                             <BsFillPersonFill style={{ background: 'rgba(33, 126, 253, 0.298039)', borderRadius: '6px', width: '30px', height: '25px', color: '#217EFD' }} />
                             <NomeUsuario>Fulano de tal</NomeUsuario>
-                            <FiArrowDownLeft />
+                            <FiArrowDownLeft style={{ background: 'rgba(253, 221, 225, 0.8)', color: '#FF324B', width: '3.2%', height: '24px', borderRadius: '10rem', marginLeft: '30rem' }} />
+                            <p style={{ color: '#FF324B' }}>+ 7,44%</p>
                         </li>
+
+                        <li style={{ listStyle: 'none', display: 'flex', marginLeft: '-6%', width: '49rem', padding: '3%', borderBottom: ' 1px solid rgba(37, 38, 40, 0.2)' }}>
+                            <BsFillPersonFill style={{ background: 'rgba(33, 126, 253, 0.298039)', borderRadius: '6px', width: '30px', height: '25px', color: '#217EFD' }} />
+                            <NomeUsuario>Fulano de tal</NomeUsuario>
+                            <FiArrowUpRight style={{ background: 'rgba(26, 213, 152, 0.2)', color: '#1AD598', width: '3.2%', height: '24px', borderRadius: '10rem', marginLeft: '30rem' }} />
+                            <p style={{ color: '#1AD598' }}>+ 7,44%</p>
+                        </li>
+
+                        <li style={{ listStyle: 'none', display: 'flex', marginLeft: '-6%', width: '49rem', padding: '3%', borderBottom: ' 1px solid rgba(37, 38, 40, 0.2)' }}>
+                            <BsFillPersonFill style={{ background: 'rgba(33, 126, 253, 0.298039)', borderRadius: '6px', width: '30px', height: '25px', color: '#217EFD' }} />
+                            <NomeUsuario>Fulano de tal</NomeUsuario>
+                            <FiArrowUpRight style={{ background: 'rgba(26, 213, 152, 0.2)', color: '#1AD598', width: '3.2%', height: '24px', borderRadius: '10rem', marginLeft: '30rem' }} />
+                            <p style={{ color: '#1AD598' }}>+ 7,44%</p>
+                        </li>
+                        <li style={{ listStyle: 'none', display: 'flex', marginLeft: '-6%', width: '49rem', padding: '3%', borderBottom: ' 1px solid rgba(37, 38, 40, 0.2)' }}>
+
+                            <BsFillPersonFill style={{ background: 'rgba(33, 126, 253, 0.298039)', borderRadius: '6px', width: '30px', height: '25px', color: '#217EFD' }} />
+                            <NomeUsuario>Fulano de tal</NomeUsuario>
+                            <FiArrowDownLeft style={{ background: 'rgba(253, 221, 225, 0.8)', color: '#FF324B', width: '3.2%', height: '24px', borderRadius: '10rem', marginLeft: '30rem' }} />
+                            <p style={{ color: '#FF324B' }}>+ 7,44%</p>
+                        </li>
+
                     </ul>
                 </div>
             </CardUsuarios>
