527c55d0eb584d5ddf10a2677f11c5ad7de93f70
Lucassec1 <106749169+Lucassec1@users.noreply.github.com>
2022-09-01 14:45:30 -0300

    up

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -3,58 +3,65 @@ import { Grafico, NomeUsuario, CardUsuarios, HeaderRenderUsusarios } from "./sty
 import Graficouse from './components/graficouse';
 import { BsFillPersonFill } from "react-icons/bs";
 import { FiArrowUpRight, FiArrowDownLeft } from "react-icons/fi";
-
-
+import api from "../../api";
+import React, { useState, useEffect } from "react";
 
 export default function Home() {
+    const [ usuarios, setUsusarios ] = useState([])
 
+    useEffect(() => {
+        api.get('/usuarios')
+            .then((response) => {
+                setUsusarios(response.data)
+                console.log("foi")
+
+            })
+            .catch(() => {
+                console.log('n foi')
+            });
+    }, []
+    )
+
+
+    var receber = [];
+    for (var user = 0; user < 8; user++) {
+        receber.push(usuarios[user])
+    }
+
+    console.log(receber)
+
+    const nome = receber.map((nameuser) => nameuser?.nome);
     return (
         <>
             <NavBarHome />
             <CardUsuarios class="row justify-content-around">
                 <div class="col-6">
                     <HeaderRenderUsusarios>Analise de usuarios</HeaderRenderUsusarios>
+
+
                     <ul>
-                        <li style={{ listStyle: 'none', display: 'flex', marginLeft: '-6%', width: '49rem', padding: '3%', borderBottom: ' 1px solid rgba(37, 38, 40, 0.2)' }}>
-                            <BsFillPersonFill style={{ background: 'rgba(33, 126, 253, 0.298039)', borderRadius: '6px', width: '30px', height: '25px', color: '#217EFD' }} />
-                            <NomeUsuario>Fulano de tal</NomeUsuario>
-                            <FiArrowUpRight style={{ background: 'rgba(26, 213, 152, 0.2)', color: '#1AD598', width: '3.2%', height: '24px', borderRadius: '10rem', marginLeft: '30rem' }} />
-                            <p style={{ color: '#1AD598' }}>+ 7,44%</p>
-                        </li>
+                        {
+                            nome.map((nome, key) => {
+                                return(
+                                <li style={{ listStyle: 'none', display: 'flex', marginLeft: '-6%', width: '49rem', padding: '3%', borderBottom: ' 1px solid rgba(37, 38, 40, 0.2)' }}>
+                                    <BsFillPersonFill style={{ background: 'rgba(33, 126, 253, 0.298039)', borderRadius: '6px', width: '30px', height: '25px', color: '#217EFD' }} />
+                                    <NomeUsuario>
 
-                        <li style={{ listStyle: 'none', display: 'flex', marginLeft: '-6%', width: '49rem', padding: '3%', borderBottom: ' 1px solid rgba(37, 38, 40, 0.2)' }}>
+                                        {nome}
 
-                            <BsFillPersonFill style={{ background: 'rgba(33, 126, 253, 0.298039)', borderRadius: '6px', width: '30px', height: '25px', color: '#217EFD' }} />
-                            <NomeUsuario>Fulano de tal</NomeUsuario>
-                            <FiArrowDownLeft style={{ background: 'rgba(253, 221, 225, 0.8)', color: '#FF324B', width: '3.2%', height: '24px', borderRadius: '10rem', marginLeft: '30rem' }} />
-                            <p style={{ color: '#FF324B' }}>+ 7,44%</p>
-                        </li>
+                                    </NomeUsuario>
+                                    <FiArrowUpRight style={{ background: 'rgba(26, 213, 152, 0.2)', color: '#1AD598', width: '3.2%', height: '24px', borderRadius: '10rem', marginLeft: '30rem' }} />
+                                    <p style={{ color: '#1AD598' }}>+ 7,44%</p>
+                                </li>
+                                )
+                            })
 
-                        <li style={{ listStyle: 'none', display: 'flex', marginLeft: '-6%', width: '49rem', padding: '3%', borderBottom: ' 1px solid rgba(37, 38, 40, 0.2)' }}>
-                            <BsFillPersonFill style={{ background: 'rgba(33, 126, 253, 0.298039)', borderRadius: '6px', width: '30px', height: '25px', color: '#217EFD' }} />
-                            <NomeUsuario>Fulano de tal</NomeUsuario>
-                            <FiArrowUpRight style={{ background: 'rgba(26, 213, 152, 0.2)', color: '#1AD598', width: '3.2%', height: '24px', borderRadius: '10rem', marginLeft: '30rem' }} />
-                            <p style={{ color: '#1AD598' }}>+ 7,44%</p>
-                        </li>
-
-                        <li style={{ listStyle: 'none', display: 'flex', marginLeft: '-6%', width: '49rem', padding: '3%', borderBottom: ' 1px solid rgba(37, 38, 40, 0.2)' }}>
-                            <BsFillPersonFill style={{ background: 'rgba(33, 126, 253, 0.298039)', borderRadius: '6px', width: '30px', height: '25px', color: '#217EFD' }} />
-                            <NomeUsuario>Fulano de tal</NomeUsuario>
-                            <FiArrowUpRight style={{ background: 'rgba(26, 213, 152, 0.2)', color: '#1AD598', width: '3.2%', height: '24px', borderRadius: '10rem', marginLeft: '30rem' }} />
-                            <p style={{ color: '#1AD598' }}>+ 7,44%</p>
-                        </li>
-                        <li style={{ listStyle: 'none', display: 'flex', marginLeft: '-6%', width: '49rem', padding: '3%', borderBottom: ' 1px solid rgba(37, 38, 40, 0.2)' }}>
-
-                            <BsFillPersonFill style={{ background: 'rgba(33, 126, 253, 0.298039)', borderRadius: '6px', width: '30px', height: '25px', color: '#217EFD' }} />
-                            <NomeUsuario>Fulano de tal</NomeUsuario>
-                            <FiArrowDownLeft style={{ background: 'rgba(253, 221, 225, 0.8)', color: '#FF324B', width: '3.2%', height: '24px', borderRadius: '10rem', marginLeft: '30rem' }} />
-                            <p style={{ color: '#FF324B' }}>+ 7,44%</p>
-                        </li>
-
+                        }
                     </ul>
                 </div>
             </CardUsuarios>
             <Grafico><Graficouse /></Grafico>
         </>
     )
+    
 }
