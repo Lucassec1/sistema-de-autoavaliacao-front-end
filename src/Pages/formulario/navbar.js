20749676709687a9697c981e6c56ab36e7fd5645
Lucasmartinsn <106749361+Lucasmartinsn@users.noreply.github.com>
2022-09-02 16:07:55 -0300

    up

---
+++ src/Pages/formulario/navbar.js
@@ -0,0 +1,32 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState } from 'react';
+import { BsArrowLeftShort } from "react-icons/bs";
+
+
+function navbargeral() {
+    function voltar() {
+        window.history.back();
+    }
+    return (
+        <div>
+            <div className='teste'>
+                <Navbar variant="primary" style={{ color: '#fff' }} expand="lg">
+                    <Container>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Collapse id="basic-navbar-nav">
+                            <Nav className="me-auto">
+                                <Nav.Link > <BsArrowLeftShort onClick={voltar} /></Nav.Link>
+                                <Nav.Link href="/criarpesquisa"> Criar Pesquisa</Nav.Link>
+                                <Nav.Link href="/minhapesquisa">Minhas Pesquisa</Nav.Link>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
+            </div>
+        </div>
+    )
+}
+export default navbargeral;
